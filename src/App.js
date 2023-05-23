2699ed7f4992efa69d6eeca1ae8114100c36a83e
kyounghye <kyoung00828@gmail.com>
2023-05-23 18:44:56 +0900

    feat:기본 라우팅 세팅

--- src/App.js
+++ src/App.js
@@ -3,49 +3,28 @@ import { Button, Nav, Navbar, Container, Row, Col } from "react-bootstrap";
 import { useState } from "react";
 import data from "./data.js";
 import { Routes, Route, Link } from "react-router-dom";
-
-function priceToString(price) {
-  return price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-}
-
-const ProductCard = ({ products }) => {
-  return products.map((item) => {
-    return (
-      <Col sm key={item.id} className="productCard">
-        <div className="product-info">
-          <img src={item.img} width="80%" />
-          <div className="product-summary">
-            <h4>{item.title}</h4>
-            <p>{item.content}</p>
-            <p>{priceToString(item.price)} ₩</p>
-          </div>
-        </div>
-      </Col>
-    );
-  });
-};
+import Home from "./page/Home";
+import Detail from "./page/Detail";
 
 function App() {
-  const [products, setProducts] = useState(data);
   return (
     <div className="App">
       <Navbar bg="dark" variant="dark" className="navbar">
         <Container>
-          <Navbar.Brand href="#home" className="navbar-logo">
-            DIP🤍RUN
+          <Navbar.Brand className="navbar-logo">
+            <Link to="/">DIP🤍RUN</Link>
           </Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Detail</Nav.Link>
+            <Link to="/">Home</Link>
+            <Link to="/detail">Detail</Link>
           </Nav>
         </Container>
       </Navbar>
       <div className="main-bg"></div>
-      <Container>
-        <Row>
-          <ProductCard products={products} />
-        </Row>
-      </Container>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/detail" element={<Detail />}></Route>
+      </Routes>
     </div>
   );
 }
---
+++ src/page/Home.js
@@ -0,0 +1,34 @@
+import { Container, Row, Col } from "react-bootstrap";
+import { useState } from "react";
+import data from "../data.js";
+import { priceToString } from "../common/functions.js";
+
+const ProductCard = ({ products }) => {
+  return products.map((item) => {
+    return (
+      <Col sm key={item.id} className="productCard">
+        <div className="product-info">
+          <img src={item.img} width="80%" />
+          <div className="product-summary">
+            <h4>{item.title}</h4>
+            <p>{item.content}</p>
+            <p>{priceToString(item.price)} ₩</p>
+          </div>
+        </div>
+      </Col>
+    );
+  });
+};
+
+const Home = () => {
+  const [products, setProducts] = useState(data);
+  return (
+    <Container>
+      <Row>
+        <ProductCard products={products} />
+      </Row>
+    </Container>
+  );
+};
+
+export default Home;
