df86b7ff728310a6e96fe88421c0129d07443577
kyoungg <123715126+kyoungg@users.noreply.github.com>
2023-05-23 14:58:48 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,52 @@
+import "./App.css";
+import { Button, Nav, Navbar, Container, Row, Col } from "react-bootstrap";
+import { useState } from "react";
+import data from "./data.js";
+
+function priceToString(price) {
+  return price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
+const ProductCard = ({ products }) => {
+  return products.map((item) => {
+    return (
+      <Col sm key={item.id} className="productCard">
+        <div className="product-info">
+          <img src={item.img} width="80%" />
+          <div className="product-summary">
+            <h4>{item.title}</h4>
+            <p>{item.content}</p>
+            <p>{priceToString(item.price)} ₩</p>
+          </div>
+        </div>
+      </Col>
+    );
+  });
+};
+
+function App() {
+  const [products, setProducts] = useState(data);
+  return (
+    <div className="App">
+      <Navbar bg="dark" variant="dark" className="navbar">
+        <Container>
+          <Navbar.Brand href="#home" className="navbar-logo">
+            DIP🤍RUN
+          </Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Detail</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+      <div className="main-bg"></div>
+      <Container>
+        <Row>
+          <ProductCard products={products} />
+        </Row>
+      </Container>
+    </div>
+  );
+}
+
+export default App;
