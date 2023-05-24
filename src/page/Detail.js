3e8e8d6263b1f279ea111f61d5f0ff462bc7968b
kyounghye <kyoung00828@gmail.com>
2023-05-24 15:16:49 +0900

    feat : useEffect로 input에 텍스트 입력시 alert 구현

--- src/page/Detail.js
+++ src/page/Detail.js
@@ -2,6 +2,7 @@ import { useParams } from "react-router";
 import { priceToString } from "../common/functions.js";
 import { useEffect, useState } from "react";
 import styled from "styled-components";
+import styles from "./Detail.module.css";
 
 const EventBox = styled.div`
   color: red;
@@ -14,11 +15,24 @@ const Detail = ({ products }) => {
   const { id } = useParams();
   const product = products.find((item) => item.id == id);
   const [limit, setLimit] = useState(true);
+  const [input, setInput] = useState("0");
+  const onChange = (evt) => {
+    setInput(evt.target.value);
+  };
+
   useEffect(() => {
     setTimeout(() => {
       setLimit(false);
     }, 2000);
-  });
+  }, []);
+
+  useEffect(() => {
+    if (isNaN(input) === true) {
+      alert("그러지 마세요 :(");
+      setInput("0");
+    }
+  }, [input]);
+
   return (
     <div className="container">
       {limit ? (
@@ -34,7 +48,8 @@ const Detail = ({ products }) => {
           <h4 className="pt-5">{product.title}</h4>
           <p>{product.content}</p>
           <p>{priceToString(product.price)} ₩</p>
-          <button className="btn btn-danger">주문하기</button>
+          <input type="text" value={input} onChange={onChange} />
+          <button className={styles.orderBtn}>Order</button>
         </div>
       </div>
     </div>
